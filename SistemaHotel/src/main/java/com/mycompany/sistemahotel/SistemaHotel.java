b98d988b78050f83abdf154f951aa43eefc86270
André  Barros <andretadeubarros@gmail.com>
2025-04-09 10:40:38 -0300

    Atualizaçao 1 - tela login
    desenvolvimento tela de login, falta a logo

---
+++ SistemaHotel/src/main/java/com/mycompany/sistemahotel/SistemaHotel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemahotel;
+
+/**
+ *
+ * @author ZOETEC
+ */
+public class SistemaHotel {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
